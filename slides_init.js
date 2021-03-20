f8e60b1a6dfc7181f3463a86e1929df35715c166
Steffen Meschkat <mesch@google.com>
2021-03-19 23:56:07 -0700

    Firebase encapsulated and wired to rest of state.
    * Firebase logic in classes FirebaseSender and FIrebaseReceiver.
    
    * Classes are instantiated, and wired to event handlers, in
      slides.js initslides().

--- slides_init.js
+++ slides_init.js
@@ -3,15 +3,6 @@ function init() {
 
   preAdjust();
   PR.prettyPrint();
-
-  const params = State.decode(window.location.search.substr(1));
-  if (params['follow']) {
-    initFirebaseFollow(params['follow']);
-  }
-
-  if (params['lead']) {
-    initFirebaseLead(params['lead']);
-  }
   
   console.log('slides init done');
 }
