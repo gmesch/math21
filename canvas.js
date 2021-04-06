a80a05230ebc411698f3e6cefb8d956e2bc3ecff
Steffen Meschkat <mesch@google.com>
2021-04-05 22:40:28 -0700

    reuse the Canvas class for canvas.html, save the canvas content to a png file

--- canvas.js
+++ canvas.js
@@ -1,76 +1,40 @@
-function drawCurve(ctx, curve) {
-  ctx.beginPath();
-  ctx.moveTo(curve[0][0], curve[0][1]);
-  for (let i = 1; i < curve.length; ++i) {
-    ctx.lineTo(curve[i][0], curve[i][1]);
-  }
-  ctx.stroke();
+function $(id) {
+  return document.getElementById(id);
 }
 
+function screen(canvas) {
+  $('screen').innerHTML = canvas.getScreen();
+}  
+
 function canvasInit() {
-  const canvas = document.getElementById('canvas');
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-  const ctx = canvas.getContext('2d');
-  ctx.lineWidth = 3;
-  ctx.lineCap = 'round';
-  ctx.lineJoin = 'round';
-  ctx.strokeStyle = 'orange';
-  
-  let drawing = false;
-  const curves = [];
-  let curve = null;
+  const canvas = new Canvas($('canvas'));
+  canvas.setShown(true);
+  screen(canvas);
 
-  draw = function() {
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    for (const curve of curves) {
-      drawCurve(ctx, curve);
-    }
-    if (curve && curve.length > 0) {
-      drawCurve(ctx, curve);
-    }
-  };
-  
-  canvas.addEventListener('mousedown', function(event) {
-    drawing = true;
-    curve = [];
-  });
-  
-  canvas.addEventListener('mouseup', function(event) {
-    drawing = false;
-    if (curve.length > 0) {
-      curves.push(curve);
-    }
-    curve = null;
-    draw();
+  $('next').addEventListener('click', function() {
+    canvas.setScreen(canvas.getScreen() + 1);
+    screen(canvas);
   });
 
-  canvas.addEventListener('mousemove', function(event) {
-    if (drawing) {
-      curve.push([event.offsetX, event.offsetY]);
-      draw();
+  $('prev').addEventListener('click', function() {
+    if (canvas.getScreen() > 0) {
+      canvas.setScreen(canvas.getScreen() - 1);
+      screen(canvas);
     }
   });
 
-  let mouseover = false;
-  canvas.addEventListener('mouseover', function(event) {
-    mouseover = true;
-  });
-  canvas.addEventListener('mouseout', function(event) {
-    mouseover = false;
-  });
-
-  document.body.addEventListener('keydown', function(event) {
-    if (mouseover) {
-      if (event.keyCode == 8) {
-        // delete
-        if (curve) {
-          curve.splice(0);
-        } else {
-          curves.pop();
-        }
-      }
-      draw();
-    }
+  $('save').addEventListener('click', function() {
+    const opts = {
+      types: [{
+        description: 'PNG',
+        accept: {'image/png': ['.png']},
+      }],
+    };
+    canvas.toBlob(async function(blob) {
+      const handle = await window.showSaveFilePicker(opts);
+      const writable = await handle.createWritable();
+      await writable.write(blob);
+      writable.close();
+    });
   });
 }
